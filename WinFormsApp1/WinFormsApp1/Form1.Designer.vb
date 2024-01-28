0808404c829ae4dc296f40d4ccfe12255dca5eab
zhang7460366 <42087330+zhang7460366@users.noreply.github.com>
2024-01-28 22:10:28 +0800

    Add files via upload

---
+++ WinFormsApp1/WinFormsApp1/Form1.Designer.vb
@@ -0,0 +1,101 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        TextBox1 = New TextBox()
+        Button1 = New Button()
+        Panel1 = New Panel()
+        Label1 = New Label()
+        Label2 = New Label()
+        Panel1.SuspendLayout()
+        SuspendLayout()
+        ' 
+        ' TextBox1
+        ' 
+        TextBox1.Font = New Font("Microsoft YaHei UI", 9F, FontStyle.Bold, GraphicsUnit.Point, CByte(134))
+        TextBox1.Location = New Point(262, 22)
+        TextBox1.Name = "TextBox1"
+        TextBox1.Size = New Size(139, 30)
+        TextBox1.TabIndex = 0
+        TextBox1.Text = "192.168.1."
+        ' 
+        ' Button1
+        ' 
+        Button1.Location = New Point(548, 20)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(112, 34)
+        Button1.TabIndex = 1
+        Button1.Text = "开始"
+        Button1.UseVisualStyleBackColor = True
+        ' 
+        ' Panel1
+        ' 
+        Panel1.BackColor = Color.White
+        Panel1.Controls.Add(Label2)
+        Panel1.Controls.Add(Label1)
+        Panel1.Controls.Add(Button1)
+        Panel1.Controls.Add(TextBox1)
+        Panel1.Location = New Point(-1, 363)
+        Panel1.Name = "Panel1"
+        Panel1.Size = New Size(695, 137)
+        Panel1.TabIndex = 5
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Location = New Point(123, 11)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(100, 24)
+        Label1.TabIndex = 2
+        Label1.Text = "检测网段："
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Location = New Point(103, 35)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(135, 24)
+        Label2.TabIndex = 3
+        Label2.Text = "如：192.168.1."
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(11F, 24F)
+        AutoScaleMode = AutoScaleMode.Font
+        BackColor = Color.Red
+        ClientSize = New Size(687, 450)
+        Controls.Add(Panel1)
+        Name = "Form1"
+        Text = "Form1"
+        Panel1.ResumeLayout(False)
+        Panel1.PerformLayout()
+        ResumeLayout(False)
+    End Sub
+
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+
+End Class
---
+++ WinFormsApp1/WinFormsApp1/Form1.vb
@@ -0,0 +1,52 @@
+﻿Imports System.Net
+Imports System.Net.NetworkInformation
+Imports System.Threading
+
+Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        For i As Integer = 0 To 255
+            Dim ip As String = TextBox1.Text & i.ToString()
+            Dim pingSender As New Ping()
+            AddHandler pingSender.PingCompleted, AddressOf PingCompleted
+            pingSender.SendAsync(ip, 100, i)
+
+        Next
+    End Sub
+
+    Private Sub PingCompleted(sender As Object, e As PingCompletedEventArgs)
+        If e.Reply.Status = IPStatus.Success Then
+            If e.Reply.RoundtripTime < 200 Then
+                Me.Invoke(Sub() UpdateLabel(e.UserState.ToString(), Color.Green))
+            Else
+                Me.Invoke(Sub() UpdateLabel(e.UserState.ToString(), Color.Red))
+            End If
+        End If
+    End Sub
+
+    Private Sub UpdateLabel(labelIndex As String, color As Color)
+        Dim label As Label = Me.Controls("Label" & labelIndex)
+        label.BackColor = color
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim x As Integer = 20
+        Dim y As Integer = 20
+        Dim labelIndex As Integer = 0
+        For i As Integer = 0 To 255
+            Dim label As New Label()
+            label.Text = i.ToString()
+            label.AutoSize = True
+            label.Location = New Point(x, y)
+            label.Name = "Label" & i.ToString()
+            label.BackColor = Color.Red
+            label.ForeColor = Color.Black
+            Me.Controls.Add(label)
+            x += 40
+            labelIndex += 1
+            If labelIndex Mod 16 = 0 Then
+                x = 20
+                y += 20
+            End If
+        Next
+    End Sub
+End Class
